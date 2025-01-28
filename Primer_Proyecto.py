f3361ee8a9d171abaac014e65b044b9a5e6605e7
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-28 18:22:26 -0300

    Add files via upload

---
+++ Primer_Proyecto.py
@@ -0,0 +1,6 @@
+'''
+Vas a crear un código en Python que le pida a tu amigo que responda dos preguntas que 
+requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras 
+combinadas, para formar una marca creativa. 
+'''
+print("la cerveza se llama : " +  " \" " + input("¿En que mes naciste? : ") + " " + input("¿Cual es tu color favorito? : ") + " \" ")
