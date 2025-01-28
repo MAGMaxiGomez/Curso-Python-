1779214a6a819ba5752a84ec2f1b79efcdc1025d
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-28 16:46:34 -0300

    Add files via upload

---
+++ Practica_Input1.py
@@ -0,0 +1,7 @@
+'''
+Práctica Input 1
+Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+¿Qué estás estudiando?
+El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+'''
+print(input("¿Qué estás estudiando? "))
