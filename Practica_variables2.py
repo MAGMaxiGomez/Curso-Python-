7222f0696d573ee5c8a0f2896ae0ad337389084a
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-28 21:10:09 -0300

    Add files via upload

---
+++ Practica_variables2.py
@@ -0,0 +1,15 @@
+'''
+Crea tres variables:
+
+1 nombre
+
+2 apellido
+
+3 nombrecompleto
+
+A nombre, asígnale el valor "Julia", y en apellido, asigna el valor "Roberts". Finalmente, 
+construye la variable nombrecompleto concatenando las variables (recuerda sumar un espacio intermedio).
+'''
+nombre = "Julia "
+apellido = "Roberts"
+nombrecompleto = nombre + apellido
