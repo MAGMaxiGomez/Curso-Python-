87b06b67427fd0d85f7653e07ab570d637b416be
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-29 15:54:49 -0300

    Add files via upload

---
+++ Practica_Integers2.py
@@ -0,0 +1,8 @@
+'''
+Práctica con Floats
+Declara una variable numérica llamada num_decimal que contenga un valor de tipo float de tu elección.
+
+Imprime el tipo de dato de dicha variable.
+'''
+num_decimal = 5.8
+print(num_decimal)
