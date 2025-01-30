eebe14d35a567325965a799cfa158a10d4262dff
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-30 00:42:34 -0300

    Add files via upload

---
+++ Practica_OpArit2.py
@@ -0,0 +1,11 @@
+'''
+Práctica Operadores Matemáticos 2
+Muestra en pantalla el módulo (es decir, el resto) de la división entre 456 y 33.
+
+Debes mostrar solo el valor numérico que resulta de esta operación.
+'''
+numero1 = 456
+numero2 = 33
+resultado = numero1 % numero2
+
+print(int(resultado))
