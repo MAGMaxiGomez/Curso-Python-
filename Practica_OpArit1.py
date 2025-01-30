ef72870fb2a79d37ffe4c7ed7d1a7d260a4ab2be
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-30 00:40:17 -0300

    Add files via upload

---
+++ Practica_OpArit1.py
@@ -0,0 +1,10 @@
+'''
+Práctica Operadores Matemáticos 1
+Muestra en pantalla el cociente (división al piso) de los siguientes dos números: 874 dividido entre 27.
+
+Debes mostrar solo el valor numérico que resulta de esta operación.
+'''
+numero1 = 874
+divisor = 27
+resultado = numero1 / divisor
+print(int(resultado))
