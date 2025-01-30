95e5949c528810a4dc4a9549229792c8e542ed56
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-30 00:52:31 -0300

    Add files via upload

---
+++ Practica_Redondeo1.py
@@ -0,0 +1,8 @@
+'''
+Práctica Redondeo 1
+Redondea el resultado de la división 10/3 a un número con 2 decimales, y muestra en pantalla el valor redondeado.
+'''
+division = 10 / 3
+division = round(division, 2)
+
+print(division)
