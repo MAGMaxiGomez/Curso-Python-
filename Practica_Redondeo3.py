4ca70548e1ef6494a1d096040521a1a61305206a
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-30 00:57:27 -0300

    Add files via upload

---
+++ Practica_Redondeo3.py
@@ -0,0 +1,8 @@
+'''
+Práctica Redondeo 3
+Calcula la raíz cuadrada de 5, y muestra en pantalla el resultado redondeado con 4 posiciones decimales.
+'''
+numero = 5**0.5
+numero = round(numero, 4)
+
+print(numero)
