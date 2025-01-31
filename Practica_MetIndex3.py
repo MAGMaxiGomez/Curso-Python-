6b344eed6ef9e9514f14da2c4a7cdc32dbc443bd
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-31 19:20:15 -0300

    Add files via upload

---
+++ Practica_MetIndex3.py
@@ -0,0 +1,9 @@
+'''
+Práctica Método Index() 3
+Encuentra y muestra en pantalla el índice de la última aparición de la palabra "práctica" en la siguiente frase:
+
+"En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+'''
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+palabra = frase.rindex("práctica")
+print(palabra)
