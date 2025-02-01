7018b67b96434862c7955ed2dafc36d61f0b1c28
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-01-31 21:53:18 -0300

    Add files via upload

---
+++ Practica_SubString2.py
@@ -0,0 +1,10 @@
+'''
+Práctica Sub-Strings 2
+Toma cada tercer caracter empezando desde el noveno hasta el final de la frase, e imprime el resultado.
+
+"Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+'''
+
+frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+caracteres = frase[9::4]
+print(caracteres)
