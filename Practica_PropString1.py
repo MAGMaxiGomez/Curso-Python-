af8eb57bed8d8487611b84c176a3aab4adc95940
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-02 12:47:08 -0300

    Add files via upload

---
+++ Practica_PropString1.py
@@ -0,0 +1,9 @@
+'''
+Práctica Propiedades de Strings 1
+Concatena 15 veces el texto "Repetición" y muestra el resultado en pantalla. 
+Por suerte, conoces que los strings son multiplicables y puedes realizar esta actividad de forma simple y elegante.
+'''
+palabra = "Repetición"
+resultado = palabra * 15
+
+print(resultado)
