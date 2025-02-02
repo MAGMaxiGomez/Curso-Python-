dee7cfef73286dd6e706ab841f900a5a79dc93f1
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-02 20:20:20 -0300

    Add files via upload

---
+++ Practica_Lista3.py
@@ -0,0 +1,11 @@
+'''
+Práctica Listas 3
+Utiliza el método pop() para quitar el tercer elemento de la siguiente lista llamada frutas, y almacénalo en una variable llamada eliminado.
+1 manzana
+2 banana
+3 mango
+4 cereza
+5 sandía
+'''
+frutas = ["manzana", "banana", "mango", "cereza", "sandía"]
+eliminado = frutas.pop(2)
