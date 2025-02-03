9624b77e8faf28a23817f74f8146675df898e291
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-02 23:55:20 -0300

    Add files via upload

---
+++ Practica_Sets3.py
@@ -0,0 +1,9 @@
+'''
+Práctica Sets 3
+Agrega el nombre Damián al siguiente set, utilizando métodos de sets:
+sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
+'''
+sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
+agregado = sorteo.add("Damián")
+
+print(sorteo)
