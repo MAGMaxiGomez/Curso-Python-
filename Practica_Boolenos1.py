b5b5e5b9f4defa1c08fd8fb5c158bb3802db28f0
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-04 14:44:52 -0300

    Add files via upload

---
+++ Practica_Boolenos1.py
@@ -0,0 +1,7 @@
+'''
+Práctica Booleanos 1
+Realiza una comparación que arroje como resultado un booleano y almacena el resultado (True/False) en una variable llamada prueba
+'''
+comparacion = 5 > 6
+prueba = comparacion
+print(prueba)
