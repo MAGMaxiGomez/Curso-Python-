d73177036697d94f5fc79d2ec20ca6ecbffaf6f9
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-07 03:30:11 -0300

    Add files via upload

---
+++ Practica_Zip2.py
@@ -0,0 +1,7 @@
+'''
+Práctica Zip 2
+Crea un objeto zip formado a partir de listas, de un conjunto de marcas y productos que tú prefieras, dentro de la variable mi_zip.
+'''
+marcas = []
+productos = []
+mi_zip = zip(marcas, productos)
