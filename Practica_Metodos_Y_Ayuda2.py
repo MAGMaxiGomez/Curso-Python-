1f7e8884fdc9a4fed361035b4cc0889f511726f4
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-08 17:25:46 -0300

    Add files via upload

---
+++ Practica_Metodos_Y_Ayuda2.py
@@ -0,0 +1,10 @@
+'''
+Práctica Métodos y Ayuda 2
+Añade el elemento "naranja" como el cuarto elemento de la siguiente lista "frutas", utilizando
+
+el método insert():
+'''
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"] 
+
+agregando = frutas.insert(3, "naranja")
+print(frutas)
---
+++ Pratica_Metodos_Y_Ayuda1.py
@@ -0,0 +1,14 @@
+'''
+Práctica Métodos y Ayuda 1
+Remueve los caracteres a la izquierda de nuestro texto principal:
+,
+:
+%
+_
+#
+Utiliza el método lstrip(). Imprime el resultado en pantalla:
+'''
+texto = ",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#"
+x = texto.lstrip(",:%_#")
+
+print(x)
