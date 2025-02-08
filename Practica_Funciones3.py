941d28c8d1af2b261f012d5d35fe893e2ee50309
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-08 19:32:45 -0300

    Add files via upload

---
+++ Practica_Funciones3.py
@@ -0,0 +1,10 @@
+'''
+Práctica Crear Funciones 3
+Declara una función llamada cuadrado, que tome como argumento un número cualquiera, 
+y que cada vez que sea llamada, imprima en pantalla el cuadrado de dicho número (es decir, la potencia 2 del valor).
+El nombre del argumento que debe tomar dicha función es un_numero. Crea dicha variable y asígnale un número cualquiera.
+Solo debes definir la función y crear la variable, no debes invocar la función luego.
+'''
+un_numero = 5
+def cuadrado(numero):
+    print(numero**2)
