d8fd2a878ddcf1ef8d243b2492084ef8bf7bb623
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-08 16:22:30 -0300

    Add files via upload

---
+++ Practica_PropString3.py
@@ -0,0 +1,7 @@
+'''
+Práctica Propiedades de Strings 3
+Muestra en pantalla el largo (en números de caracteres) de la palabra electroencefalografista.
+'''
+palabra = "electroencefalografista"
+
+print(len(palabra))
