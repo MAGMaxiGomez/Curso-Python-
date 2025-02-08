4b29a406cbc9db3074fd5d06657d099c898f0318
Dev Gomez Maxi <magmaxigomez@gmail.com>
2025-02-08 16:18:47 -0300

    Add files via upload

---
+++ Practica_integers3.py
@@ -0,0 +1,13 @@
+'''
+Práctica con Tipos de Datos Numéricos
+¿De qué tipo es el resultado de la suma de 7.5 + 2.5? Genera el código para verificarlo.
+Para ello, crea dos variables:
+1  num1 = 7.5
+2  num2 = 2.5
+A continuación, muestra en pantalla el tipo de dato que resulta de la suma de ambos números.
+'''
+num1 = 7.5
+num2 = 2.5
+resultado = num1 + num2
+
+print(type(resultado))
